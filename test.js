5016a6e4d34d52116aaa3c0ecc6a4805805d3260
EilteByte <mario.figueroa87@gmail.com>
2017-07-19 02:18:19 -0700

    Added Alpha Position Functionality

--- test.js
+++ test.js
@@ -36,7 +36,7 @@ var config = {
 		},
 		{
 			"module": "currentweather",
-			"position": "lower_center",
+			"position": "top_right",
 			"config": {
 				"units": "imperial",
 				"location": "Ladera Ranch",
@@ -46,10 +46,10 @@ var config = {
 		},
 		{
 			"module": "weatherforecast",
-			"position": "top_center",
+			"position": "top_right",
 			"header": "Weather Forecast",
 			"config": {
-				"units": "Hello",
+				"units": "testing",
 				"location": "Ladera Ranch",
 				"locationID": "5364199",
 				"appid": "0d8cbd2c42f528549ddb896767e82809"
