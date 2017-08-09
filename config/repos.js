6daad3127b6e5501ec41cd8d55d11bb2e2fc823d
EilteByte <mario.figueroa87@gmail.com>
2017-08-09 16:41:30 -0700

    Lower_Center = Lower_Third

--- config/repos.js
+++ config/repos.js
@@ -1,11 +1,3 @@
 var reposits = 
 {
-	"MagicMirrorConfigurator": {
-		"url": "https://github.com/EliteByte/MagicMirrorConfigurator",
-		"hash": "0224a741ad0c4a1e3c06c1f344b8df462f9d70e5"
-	},
-	"MMM-bitcoin": {
-		"url": "https://github.com/valmassoi/MMM-bitcoin",
-		"hash": "f9b7ad18d56d72c92f577cf8c6ba51d6a8877ddb"
-	}
 }
