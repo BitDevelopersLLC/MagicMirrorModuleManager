b4f3d92d7b9abb468f8ca4f3d2ab2bca60df17e8
EilteByte <mario.figueroa87@gmail.com>
2017-08-10 15:02:10 -0700

    Added Hello World Config
    The Hello World module is by default inside of the MagicMirror. So I will be using the module to display the ModuleManager IP.

--- config/config.js
+++ config/config.js
@@ -1,81 +1,10 @@
-/* Magic Mirror Config Sample
- *
- * By Michael Teeuw http://michaelteeuw.nl
- * MIT Licensed.
- */
 
-var config = {
-	port: 8080,
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],
-
-	language: 'en',
-	timeFormat: 24,
-	units: 'imperial ',
-
-	modules: [
-		{
-			module: 'alert',
-		},
-		{
-			module: "updatenotification",
-			position: "top_bar"
-		},
-		{
-			module: 'clock',
-			position: 'top_left'
-		},
-		{
-			module: 'calendar',
-			header: 'US Holidays',
-			position: 'top_left',
-			config: {
-				maximumNumberOfDays: 90,
-				calendars: [
-					{
-						symbol: 'calendar-check-o ',
-						url: 'webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics'
-					}
-				]
-			}
-		},
-		{
-			module: 'currentweather',
-			position: 'top_right',
-			config: {
-				units: 'imperial',
-				location: 'Ladera Ranch',
-				locationID: '5364199',  //ID from http://www.openweathermap.org
-				appid: '0d8cbd2c42f528549ddb896767e82809'
-			}
-		},
-		{
-			module: 'weatherforecast',
-			position: 'top_right',
-			header: 'Weather Forecast',
-			config: {
-				units: 'imperial',
-				location: 'Ladera Ranch',
-				locationID: '5364199',  //ID from http://www.openweathermap.org
-				appid: '0d8cbd2c42f528549ddb896767e82809'
-			}
-		},
-		{
-			module: 'newsfeed',
-			position: 'bottom_bar',
-			config: {
-				feeds: [
-					{
-						title: "New York Times",
-						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-					}
-				],
-				showSourceTitle: true,
-				showPublishDate: true
-			}
-		},
-	]
-
-};
-
-/*************** DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== 'undefined') {module.exports = config;}
+var infoMMMM = 
+	{
+		module: "helloworld",
+		position: "bottom_bar",	// This can be any of the regions.
+		config: {
+			// See 'Configuration options' for more information.
+			text: "{PLACE HOLDER}"
+		}
+	}
